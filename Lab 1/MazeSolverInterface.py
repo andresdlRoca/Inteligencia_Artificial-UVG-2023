ac87bea7cdbb4f86fea9fba2a457b994eae4db16
andresdlRoca <andresdelaroca11@gmail.com>
2023-01-22 12:42:26 -0600

    Avances en la resolucion de BFS

--- Lab 1/MazeSolverInterface.py
+++ Lab 1/MazeSolverInterface.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MazeSolver:
     def __init__(self, maze):
         self.maze = maze
@@ -12,4 +14,76 @@ class MazeSolver:
         else:
             # Maze will be displayed here via print and saving an image to the directory
             pass
+    
+    def coordinateFinder(self, color):
+        im = np.array(self.maze)
+        X, Y = np.where(np.all(im==color, axis=2))
+        zippedCords = np.column_stack((X,Y))
+        return(zippedCords)
 
+class DepthFirstSolver(MazeSolver):
+    def __init__(self, maze):
+        super().__init__(maze)
+
+    def solve(self):
+        pass #Here we solve the maze via Depth First Algorithm
+
+class BreadthFirstSolver(MazeSolver):
+    def __init__(self, maze):
+        super().__init__(maze)
+
+    def solve(self, start, end):
+        #pass #Here we solve the maze via Breadth First Algorithm
+        solutionMatrix = []
+        for x in range(len(self.maze)):
+            solutionMatrix.append([])
+            for y in range(len(self.maze[x])):
+                solutionMatrix[-1].append(0)
+        x, y = start
+        solutionMatrix[x][y] = 1
+        step = 0
+        while solutionMatrix[end[0]][end[1]] == 0:
+            step +=1
+            self.take_step(self.maze, solutionMatrix, step)
+        x,y = end
+        step = solutionMatrix[x][y]
+        shortestPath = [(x,y)]
+        while step > 1:
+            if x > 0 and solutionMatrix[x-1][y] == step-1:
+                x, y = x-1, y
+                shortestPath.append((x,y))
+    
+    def take_step(maze, matrix, step):
+        for x in range(len(matrix)):
+            for y in range(len(matrix[x])):
+                if matrix[x][y] == step:
+                    if x>0 and matrix[x-1][y] == 0 and maze[x-1][y] == 0:
+                        matrix[x-1][y] = step + 1
+                    if y>0 and matrix[x][y-1] == 0 and maze[x][y-1] == 0:
+                        matrix[x][y-1] = step+1
+                    if x<len(matrix)-1 and matrix[x+1][y] == 0 and maze[x+1][y] == 0:
+                        matrix[x+1][y]=step+1
+                    if y<len(matrix[1])-1 and matrix[x][y+1] == 0 and maze[x][y+1] == 0:
+                        matrix[x][y+1] = step+1
+        
+
+#Possible heuristic functions for A* algorithms:
+# 1. Euclidian distance
+# 2. Manhattan distance
+# 3. Great Circle distance
+
+class A1Solver(MazeSolver):
+    def __init__(self, maze):
+        super().__init__(maze)
+
+    def solve(self):
+        pass #Here we solve the maze via A* algorithm
+
+class A2Solver(MazeSolver):
+    def __init__(self, maze):
+        super().__init__(maze)
+
+    def solve(self):
+        pass #Here we solve the maze via A* algorithm
+
+    
--- Lab 1/main.py
+++ Lab 1/main.py
@@ -1,9 +1,35 @@
 from imageReader import ImageReader
-
-imgReader = ImageReader("lab1.bmp", 50,50)
+import numpy as np
+import MazeSolverInterface
+green = [0,255,0] #Goal
+red = [254,6,6] #Start
+imgReader = ImageReader("lab1.bmp", 50, 50)
 
 imageMatrix = imgReader.image2Matrix()
 
-print(imageMatrix)
+# print(imageMatrix[5])
+
+problemSolver = MazeSolverInterface
+problemSolver(imageMatrix)
+
+goals = problemSolver.coordinateFinder(green)
+start = problemSolver.coordinateFinder(red)
+
+BFSsolver = problemSolver.BreadthFirstSolver.solve(imageMatrix, start=start, end=goals)
+
+
+# # Unique elements in nested tuple
+# # Using nested loop + set()
+# res = []
+# temp = set()
+# for inner in imageMatrix:
+#         for ele in inner:
+#             if not ele in temp:
+#                 temp.add(ele)
+#                 res.append(ele)
+ 
+# # printing result
+# print("Unique elements in nested tuples are : " + str(res))
+
 
 
